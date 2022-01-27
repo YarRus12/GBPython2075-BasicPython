d343c9b42f9941150f5202bccee8ca270ddaa880
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-01-27 16:03:09 +1000

    Выполнение первого домашнего задания. В первой задаче схалтурил, сначала решил несколькими функциями, а потом что по условию должна быть только одна функция "convert_time" переписывать было лень поэтому запихал готовый код в "convert_time". Зато понял как работает return во вложенных функциях

---
+++ Russu_Yaroslav_dz_1/task_1_1.py
@@ -0,0 +1,35 @@
+def convert_time(duration: int) -> str:
+    def seconds(duration):
+        s = duration
+        return f'{s} сек'
+
+    def minutes(duration):
+        m = duration//60
+        s = duration%60
+        return f'{m} мин {s} сек'
+
+    def hours(duration):
+        h = duration // 3600
+        m = (duration - (h*3600))//60
+        s = duration % 60
+        return f'{h} час {m} мин {s} сек'
+
+    def days(duration):
+        d = duration // 86400
+        h = (duration - d*86400) // 3600
+        m = (duration - (d*86400) - (h*3600))//60
+        s = duration % 60
+        return f'{d} дн {h} час {m} мин {s} сек'
+
+    if duration >= 86400:
+        return days(duration)
+    elif duration >= 3600:
+        return hours(duration)
+    elif duration >= 60:
+        return minutes(duration)
+    else:
+        return seconds(duration)
+
+duration = 400153
+result = convert_time(duration)
+print(result)
