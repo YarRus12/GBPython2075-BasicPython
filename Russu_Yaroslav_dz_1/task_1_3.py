678a61430207db57af5c0411276310f7e6a6aa3c
YarRus12 <yaroslav.russu@mail.ru>
2022-01-29 09:58:00 +1000

    updated according to recommendations problem with variables in second task will be solved latter

--- Russu_Yaroslav_dz_1/task_1_3.py
+++ Russu_Yaroslav_dz_1/task_1_3.py
@@ -1,12 +1,10 @@
 def transform_string(number: int) -> str:
     if number == 1  or (number > 20 and number%10 == 1):
-        result = f'{number} процент'
+        return f'{number} процент'
     elif (number > 1 and number < 5) or (number > 20 and number%10 > 1 and number%10 <5):
-        result = f'{number} процента'
+        return f'{number} процента'
     else:
-        result = f'{number} процентов'
-    return result
-
+        return f'{number} процентов'
 
 for n in range(1, 101):
     print(transform_string(n))
