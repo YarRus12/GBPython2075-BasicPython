306ac78628d6e737eb7b449179544e2374d57344
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-07 15:55:33 +1000

    Основная часть работы завершена

--- Russu_Yaroslav_dz_3/task_3_3.py
+++ Russu_Yaroslav_dz_3/task_3_3.py
@@ -15,12 +15,15 @@
 Можно ли использовать словарь в этом случае?
 ВНИМАНИЕ! Используйте стартовый код для своей реализации"""
 
-def thesaurus(*args) -> dict:
-    # пишите свою реализацию здесь
+def thesaurus_adv(*args) -> dict:
     dict_out = {}  # результирующий словарь значений
+    letters = [i[0] for i in args]
+    names = [i for i in args]
+    for i in range(len(names)):
+        dict_out.setdefault(letters[i], [])
+        if letters[i] in dict_out:
+            dict_out[letters[i]].append(names[i])
+        else:
+            dict_out.setdefault(letters[i], names[i])
     return dict_out
-
-
 print(thesaurus("Иван", "Мария", "Петр", "Илья"))
-
-
--- Russu_Yaroslav_dz_3/task_3_4.py
+++ Russu_Yaroslav_dz_3/task_3_4.py
@@ -18,10 +18,19 @@
 Как поступить, если потребуется сортировка по ключам?"""
 
 
-def thesaurus(*args) -> dict:
-    # пишите свою реализацию здесь
-    dict_out = {}  # результирующий словарь значений
+def thesaurus_adv(*args) -> dict:
+    dict_out = {}
+    letters = [i[0] for i in args]
+    names = [i for i in args]
+    divided_names = [i.split(' ') for i in names]
+    surname_letter = list(set([i[1][0] for i in divided_names]))
+    for i in range(len(surname_letter)):
+        dict_out.setdefault(surname_letter[i], {})
+    for x in range(len(divided_names)):
+        dict_out[surname_letter].setdefault(divided_names[0][i], [)
+    #    if surname_letter[i] in dict_out:
+    #        dict_out[surname_letter[i]].append(' '.join(divided_names[i]))
+    #    else:
+    #        dict_out.setdefault(surname_letter[i], names[i])
     return dict_out
-
-
-print(thesaurus("Иван", "Мария", "Петр", "Илья"))
+print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
