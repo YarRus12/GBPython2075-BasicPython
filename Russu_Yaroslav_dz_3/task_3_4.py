44373442575d5c919eea990eef0955468549bf61
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-07 21:25:09 +1000

    Задача 5 доработана, выполнено документирование Выполнена усложненная задача с флагом

--- Russu_Yaroslav_dz_3/task_3_4.py
+++ Russu_Yaroslav_dz_3/task_3_4.py
@@ -1,16 +1,16 @@
-"""Боюсь, что вложенные словари, для меня преждевременны
+"""Боюсь, что вложенные словари, для меня преждевремены
 Дорешаю немного позже"""
 
 
-def thesaurus_adv(*args) -> dict:
-    dict_out = {}
-    letters = [i[0] for i in args]
-    names = [i for i in args]
-    divided_names = [i.split(' ') for i in names]
-    surname_letter = list(set([i[1][0] for i in divided_names]))
-    for i in range(len(surname_letter)):
-        dict_out.setdefault(surname_letter[i], {})
-    return dict_out
-print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
-"""Боюсь, что вложенные словари, для меня преждевременны
+#def thesaurus_adv(*args) -> dict:
+#    dict_out = {}
+#    letters = [i[0] for i in args]
+#    names = [i for i in args]
+#    divided_names = [i.split(' ') for i in names]
+#    surname_letter = list(set([i[1][0] for i in divided_names]))
+#    for i in range(len(surname_letter)):
+#        dict_out.setdefault(surname_letter[i], {})
+#    return dict_out
+#print(thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева"))
+"""Боюсь, что вложенные словари, для меня преждевремены
 Дорешаю немного позже"""
