483e97a80134250f2a66b50c79b941b18d90207e
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-07 20:18:47 +1000

    В задаче 2 обработано исключение AttributeError, а также обрабатывается случаи когда вторая и третья буквы написаны в верхнем регистре (принодим все к нижнему)

--- Russu_Yaroslav_dz_3/task_3_2.py
+++ Russu_Yaroslav_dz_3/task_3_2.py
@@ -18,12 +18,17 @@ def num_translate_adv(value: str) -> str:
             потом обращается к внешнему словарю по ключу, именованному как value и возвращает перевод слова на русский язык с заглавной буквой в начале,
             если ключ есть в словаре, есть ключа нет в словаре возвращает None"""
     if value[0] == value[0].upper():
-        str_out = eng_rus_dict.get(value.lower(), None).capitalize()
+        try:
+            str_out = eng_rus_dict.get(value.lower(), None).capitalize()
+        except AttributeError:  # str_out по дефолту None, поэтому обязательно обрабатываем AttributeError
+            str_out = None
     else:
-        str_out = eng_rus_dict.get(value, None)
+        str_out = eng_rus_dict.get(value.lower(), None)
     return str_out
 
 print(num_translate_adv("One"))
+print(num_translate_adv("tWo"))
+print(num_translate_adv("Twenty"))
 print(num_translate_adv("eight"))
 print(num_translate_adv("eleven"))
 
