f68df34c1eb4e495ee874fe15e8edebc43ebf358
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-14 10:58:59 +1000

    Добавлены комментарии к первой и второй задачам. Выполнена задача №3

--- Russu_Yaroslav_dz_5/task_05_02.py
+++ Russu_Yaroslav_dz_5/task_05_02.py
@@ -1,8 +1,8 @@
 
 def odd_nums(number: int) -> int:
     """Генератор, принимающий в себя n значение и возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
-    pass
-    return (x for x in range(1, number+1, 2))
+    # pass заглушка больше не нужна
+    return (x for x in range(1, number+1, 2))  # Возвращает весь генератор
 
 n = 15
 generator = odd_nums(n)
