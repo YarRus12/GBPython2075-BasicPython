b1ce5d95b828065aadbacd7c17c8ea4cd0af113c
YarRus12 <yaroslav.russu@mail.ru>
2022-02-14 20:28:27 +1000

    Задания выполнены

--- Russu_Yaroslav_dz_5/task_05_02.py
+++ Russu_Yaroslav_dz_5/task_05_02.py
@@ -1,6 +1,6 @@
 
 def odd_nums(number: int) -> int:
-    """Генератор, принимающий в себя n значение и возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
+    """Генератор, принимающий в себя n значение и возвращающий весь генератор нечетных целых чисел от 1 до number (включительно)"""
     # pass заглушка больше не нужна
     return (x for x in range(1, number+1, 2))  # Возвращает весь генератор
 
@@ -9,5 +9,7 @@ generator = odd_nums(n)
 for _ in range(1, n + 1, 2):
     print(next(generator))
 
-
 #next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
+
+print('END')
+print()
