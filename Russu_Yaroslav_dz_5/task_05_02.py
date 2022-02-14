459df2fc8a3f16238c1f8bc9e7109de2747e0105
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-14 16:34:35 +1000

    Выполнено задание №4. Необходимо поработать над оптимизацией скорости и памяти.

--- Russu_Yaroslav_dz_5/task_05_02.py
+++ Russu_Yaroslav_dz_5/task_05_02.py
@@ -8,4 +8,6 @@ n = 15
 generator = odd_nums(n)
 for _ in range(1, n + 1, 2):
     print(next(generator))
+
+
 #next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
