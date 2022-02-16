7014cf2921817dfbe4722f29546dc033a52b6e7f
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-16 16:33:09 +1000

    Выполнены задачи 1 и 2

--- Russu_Yaroalsv_dz_6/task_6_2.py
+++ Russu_Yaroalsv_dz_6/task_6_2.py
@@ -1,5 +1,4 @@
 
-from pprint import pprint
 def get_parse_attrs(line: str) -> tuple:
     """Функция принимает в себя строку и разбивает ее строковыми методами на переменные.
     объединяет их в кортежи и возвращает кортеж"""
@@ -17,7 +16,20 @@ with open('nginx_logs.txt', 'r', encoding='utf-8') as fr:
     for line in fr:
         list_out.append(get_parse_attrs(line))
 
-pprint(list_out)
-#print(type(list_out))#проверяем, что функция выдает <class 'list'>
-#print(type(list_out[0]))#проверяем, что result состоит из элементов <class 'tuple'>
+def max_ip(uniq_ip: set, ip_all: list) -> tuple:
+    """Функция определяет элемент списка с максимальным количеством вхождений
+    На вход функция принимает сет уникальных значений и список всех ip адресов
+    На выход возвращает кортеж из ip с максимальным количеством вхождений и число его вхождений"""
+    max_ip = []
+    max_number = 0
+    for i in uniq_ip:
+        number = ip_all.count(i)
+        if number > max_number:
+            max_ip, max_number = i, number
+    return (max_ip, max_number)
+
+uniq_ip = set([i[0] for i in list_out])
+ip_all = list([i[0] for i in list_out])
+ip, number = max_ip(uniq_ip, ip_all)
+print(f'Самый главный спамер это пользователь с id: {ip}, его запросы встречаются {number} раз')
 
