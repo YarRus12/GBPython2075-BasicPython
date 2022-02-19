60e5376b50a90dfeece0f1009cd3d15f55d8d3c2
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-19 20:09:59 +1000

    Переделал задачу 3 согласно условию ТЗ Теперь функция ищет файлы html и копирует в temporary только их

--- Russu_Yaroslav_dz_7/task_7_3.py
+++ Russu_Yaroslav_dz_7/task_7_3.py
@@ -2,26 +2,17 @@ import os
 
 def move_direction(name_old_dir, name_new_dir, direction = os.path.dirname(os.path.abspath(__file__))):
     """Функция принимает в себя папку источник и название новой папки
-    и переносит файлы папки источника в новую папку
-    !!! К сожалению она не отлажена, да она выполняет поставленную задачу,
-    но без обработки FileExistsError выдает ошибку!!!"""
+    ищет среди полных путей всех файлов файлы в разрешением html и папки с найденными
+     файлами в новую папку """
     import shutil
     old_dir = os.path.join(direction, name_old_dir)
     new_dir = os.path.join(direction, name_new_dir)
-    files = os.listdir(old_dir)
-    try:
-        for folder in files:
-            if folder != str(name_new_dir):
-                if os.path.exists(new_dir):
-                    os.makedirs(new_dir, exist_ok=True)
-                    shutil.copytree(old_dir, new_dir, symlinks=False)
-                    print(f'Папка {folder} скопирована в {new_dir}')
-                else:
-                    shutil.copytree(old_dir, new_dir, symlinks=False)
-                    print(f'Папка {folder} скопирована в {new_dir}')
-    except FileExistsError:
-        print(f'А как бороться с этой бедой я узнаю в следующей серии')
-
+    for root, dirs, files in os.walk(old_dir):
+        for file in files:
+            if (file.endswith(".html")):
+                dir = ((os.path.join(root,file)))
+                html_dir = dir[:(dir.find('templates'))+len('templates')]
+                shutil.copytree(html_dir, new_dir, symlinks=False, dirs_exist_ok=True)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 move_direction('my_project', 'my_project/templates')
 
