e23daadcf3c6bd69a628b4846feabb8bc43dd449
YarRus12 <yaroslav.russu@mail.ru>
2022-02-22 20:50:33 +1000

    Задание 2 исправлено согласно замечаниям

--- Russu_Yaroslav_dz_2/task_2_1.py
+++ Russu_Yaroslav_dz_2/task_2_1.py
@@ -1,6 +1,10 @@
 def result_type(expressions):
     for expression in expressions:
         print(f'{expression} - ', type(expression))
+        print(isinstance(expression, int))
+        print(dir(expression))
+
 
 expressions = 15 * 3, 15 / 3, 15 // 2, 15 ** 2
 result_type(expressions)
+print()
