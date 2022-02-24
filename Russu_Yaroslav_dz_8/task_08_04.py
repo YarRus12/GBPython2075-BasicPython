ce639baf1054139af466a6f3ac1635cf1b952082
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-24 18:05:49 +1000

    Задание 2 выполнено. Подготовлена заготовка к заданию 4

--- Russu_Yaroslav_dz_8/task_08_04.py
+++ Russu_Yaroslav_dz_8/task_08_04.py
@@ -13,3 +13,24 @@ ValueError: wrong val -5
 
 ВНИМАНИЕ! Используйте стартовый код для своей реализации:
 """
+def type_logger(func):
+    def class_type(*args):
+        result = [type(i) for i in args]
+        print(*zip(args, result), sep=', ')
+    return class_type
+
+def val_checker(func):
+    def checker(args):
+        if len(args) == len([int(i) for i in args]):
+            pass
+        else:
+            raise ValueError("Напишите имя проекта, и через запятую можете указать другую директорию")
+        return checker
+@val_checker(val_checker)  # не забудьте про аргумент-функцию
+def calc_cube(x):
+    """Возведение числа в третью степень"""
+    return x ** 3
+
+if __name__ == '__main__':
+    print(calc_cube(5))
+    print(calc_cube('ss'))
