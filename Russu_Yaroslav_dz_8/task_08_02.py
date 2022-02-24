7c51b3ac60f7e78e955f49f80cf9893544f24108
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-24 12:56:03 +1000

    Сделал заготовку к заданию 2 в лоб

--- Russu_Yaroslav_dz_8/task_08_02.py
+++ Russu_Yaroslav_dz_8/task_08_02.py
@@ -5,3 +5,25 @@
 raw = '188.138.60.101 - - [17/May/2015:08:05:49 +0000] "GET /downloads/product_2 HTTP/1.1" 304 0 "-" "Debian APT-HTTP/1.3 (0.9.7.9)"'
 parsed_raw = ('188.138.60.101', '17/May/2015:08:05:49 +0000', 'GET', '/downloads/product_2', '304', '0')
 Примечание: вы ограничились одной строкой или проверили на всех записях лога в файле? Были ли особенные строки? Можно ли для них уточнить регулярное выражение?"""
+
+import os
+import re
+
+def log_pasing(folder):
+    with open(folder, 'r', encoding='utf-8') as logs:
+        for i in logs:
+            parsed_raw = []
+            trush = []
+            remote_addr = re.findall(r"^(\S+)", i)
+            request_datetime = re.findall(r"\[([\w:/]+\s[+\-]\d{4})\]", i)
+            request_type = re.findall(r'"(\S+)', i)[0]
+            requested_resource = (re.findall(r'/(\S+)\s*(\S+)?\s*"', i)[1])[0]
+            response_code = (re.findall(r'" (.+) "', i)[0])[0:3]
+            response_size = ((re.findall(r'" (.+) "', i))[0])[1]
+            parsed_raw = remote_addr, request_datetime, request_type, requested_resource, response_code, response_size
+            print(parsed_raw)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+folder_name = 'nginx_logs.txt'
+path = os.path.join(BASE_DIR, folder_name)
+log_pasing(path)
