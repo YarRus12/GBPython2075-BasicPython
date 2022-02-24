b745b268a38d75969eef735159af1738a7598054
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-02-24 10:54:53 +1000

    Концептуально не изменилось

--- Russu_Yaroslav_dz_2/task_2_2_and_3.py
+++ Russu_Yaroslav_dz_2/task_2_2_and_3.py
@@ -1,28 +1,28 @@
+
 def convert_list_in_str(list_in: list) -> str:
     n_list = []
+    extra = 0
     for i in list_in:
         str_out = []
         if i.isdigit() == True and len(str(i)) < 2:
-            str_out.append('"')
-            str_out.append('0' + i)
-            str_out.append('"')
+            i = f'"{"0"+i}"'
+            str_out += ''.join(i)
         elif i.isdigit() == True:
-            str_out.append('"')
-            str_out.append(i)
-            str_out.append('"')
+            i = f'"{i}"'
+            str_out += ''.join(i)
         else:
             if i.isalpha():
-                str_out.append(i)
+                str_out += ''.join(i)
             else:
                 out = []
                 for j in i:
                     if j.isdigit() == True:
-                        out.append('0' + j)
+                        j = f'{"0"+j}'
+                        out.append(j)
                     else:
                         out.append(j)
-                str_out.append(''.join('"'))
-                str_out.append(''.join(out))
-                str_out.append(''.join('"'))
+                line = f'"{"".join(out)}"'
+                str_out += ''.join(line)
         n_list.append(''.join(str_out))
     return n_list
 
