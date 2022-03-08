22f8912f58b9c4c48ec5174e8a24aa09562ce1e7
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-03-09 09:35:19 +1000

    Задание 2.

--- Russu_Yaroslav_dz_11/task_11_02.py
+++ Russu_Yaroslav_dz_11/task_11_02.py
@@ -1,4 +1,16 @@
-"""Создать собственный класс-исключение, обрабатывающий ситуацию деления на ноль.
 
-Проверить его работу на данных, вводимых пользователем.
-При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой."""
+class DivisionByZero:
+    def __init__(self, divider, dividend):
+        self.divider = divider
+        self.dividend = dividend
+
+    def divide_by_null(divider, dividend):
+        try:
+            return (divider / dividend)
+        except :
+            return (f"Ошибка! Деление на ноль недопустимо")
+
+
+print(DivisionByZero.divide_by_null(100, 0))
+print(DivisionByZero.divide_by_null(100, 1))
+print(DivisionByZero.divide_by_null(100, 3))
