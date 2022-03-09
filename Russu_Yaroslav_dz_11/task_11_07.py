5200a20f86118baa3b73055e75f2edd347b24d38
YarRus12 <97214900+YarRus12@users.noreply.github.com>
2022-03-09 17:28:17 +1000

    Задание 7.

--- Russu_Yaroslav_dz_11/task_11_07.py
+++ Russu_Yaroslav_dz_11/task_11_07.py
@@ -1,8 +1,24 @@
-"""Задание 7
-Реализовать проект Операции с комплексными числами.
 
-Создать класс Комплексное число.
-Реализовать перегрузку методов сложения и умножения комплексных чисел.
-Проверить работу проекта. Для этого создать экземпляры класса (комплексные числа),
-выполнить сложение и умножение созданных экземпляров.
-Проверить корректность полученного результата."""
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.x = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма x1 и x2 равна')
+        return f'x = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение x1 и x2 равно')
+        return f'x = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'x = {self.a} + {self.b} * i'
+
+
+z_1 = ComplexNumber(5, -2)
+z_2 = ComplexNumber(7, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
